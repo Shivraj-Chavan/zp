25fc5fa6180fe8897499165ed954f37972cbb0f0
Shivraj Chavan <shivrajchavan1902@gmail.com>
2025-03-21 01:17:38 +0530

    language setup

---
+++ src/i18n.js
@@ -0,0 +1,39 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+const loadTranslations = async (lng) => {
+  const common = await import(`./locales/${lng}/common.json`);
+  const auth = await import(`./locales/${lng}/auth.json`);
+  const user = await import(`./locales/${lng}/user.json`);
+  const admin = await import(`./locales/${lng}/admin.json`);
+
+  return {
+    common: common.default,
+    auth: auth.default,
+    user: user.default,
+    admin: admin.default,
+  };
+};
+
+i18n
+  .use(LanguageDetector) 
+  .use(initReactI18next) 
+  .init({
+    lng: localStorage.getItem("language") || "en", 
+    fallbackLng: "en",
+    resources: {},
+    interpolation: {
+      escapeValue: false, 
+    },
+  });
+
+i18n.on("languageChanged", async (lng) => {
+  const translations = await loadTranslations(lng);
+  i18n.addResources(lng, "common", translations.common);
+  i18n.addResources(lng, "auth", translations.auth);
+  i18n.addResources(lng, "user", translations.user);
+  i18n.addResources(lng, "admin", translations.admin);
+});
+
+export default i18n;
